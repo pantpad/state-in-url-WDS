8a463105b02c334592c239bd3d04bf6502e2c1c1
pantpad <138708449+pantpad@users.noreply.github.com>
2024-04-21 12:30:12 +0200

    feat: add state to url using history + url searchparams

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import "./App.css";
 import CurrentPage from "./components/CurrentPage";
 import Filters from "./components/Filters";
@@ -9,12 +10,20 @@ import { mockList as allItems } from "./mockItems/list";
 export default function App() {
   console.log(allItems);
 
+  const url = new URL(document.location.href);
+  console.log(url.pathname);
+
+  function addSearchParam(name: string, value: string) {
+    url.searchParams.set(name, value);
+    window.history.replaceState(null, "null", url);
+  }
+
   return (
     <>
       <main className="min-w-[min(32rem,100vw-2rem)] py-2 px-3 bg-red-100 flex flex-col gap-2 text-slate-600">
         <Nav />
         <CurrentPage />
-        <Filters />
+        <Filters addSearchParam={addSearchParam} />
         <List list={allItems} />
       </main>
     </>
